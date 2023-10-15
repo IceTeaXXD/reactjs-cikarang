9cd88a647b6c14d2244f083e22d8e5391d4404ce
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2023-10-15 20:01:46 +0700

    feat: firebase deployment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [location, setLocation] = useState('loading');
+  const [location, setLocation] = useState("loading");
+  const [numVisit, setNumVisit] = useState(0);
 
   useEffect(() => {
     const getLocation = async () => {
-      let result = await axios.get('http://localhost:3001/location');
-      setLocation(result.data.location);
+      let result = await axios.get(
+        "https://express-cikarang.vercel.app/visitor"
+      );
+      setLocation(result.data.your_ip);
+      setNumVisit(result.data.visitor_number);
     };
 
     getLocation();
@@ -22,7 +26,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Typography variant="h4">{location}</Typography>
+        <Typography variant={"h4"}>Your IP Address is {location}.</Typography>
+        <Typography variant={"h5"}>
+          This site has been visited {numVisit} times.
+        </Typography>
       </header>
     </div>
   );
